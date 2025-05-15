9da60f4c98f73147f5e0588a0fb51f1f148d32a9
abdul-wahab <ridwonwahab@gmail.com>
2025-05-15 05:50:48 +0100

    added more templates for the web

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .import views
+
+#url configuration
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('index/', views.index, name='index'),
+    path('shop_detail/', views.shopDeatil, name='shopDetail'),
+    path('shop_listing/', views.shopListing, name='listing'),
+
+
+    path('home/', views.home)
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+#creating request handler
+
+def post_list(request):
+    #return HttpResponse('Hello worldddddk')
+    return render(request, 'blog/post_list.html', {})
+
+def index(request):
+    return render(request, 'blog/index.html', {})
+
+def shopDeatil(request):
+    return render(request, 'blog/shop-detail.html', {})
+
+def shopListing(request):
+    return render(request, 'blog/shop-listing.html', {})
+
+
+def home(request):
+    return HttpResponse('Shout out to Hiit students')
+
